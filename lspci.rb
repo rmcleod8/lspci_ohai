b59ec9bdf57f3508a6dc7ff20e89383cc3299e22
rmcleod8 <richard.mcleod@gmail.com>
2016-06-26 22:29:26 -0400

    now properly hashing lspci -vmm

--- lspci.rb
+++ lspci.rb
@@ -1,16 +1,19 @@
 # An attempt at an ohai plugin for lspci
 # 
-# 
+#
 require 'pathname'
-require 'mash'
 lspci = Pathname.new("/sbin/lspci")
-rex = Regexp.new(/"([^"]+)"/)
-devices = Mash.new
+output = Hash.new
 if lspci.exist?() && lspci.executable?()
-  `/sbin/lspci -m`.each_line do |line|
-#     rexd = line.split(/"([^"]+)"/)
-     rexd = line.split(rex)
-     output = devices[rexd[1]] = { "devicename" => rexd[4], "type" => rexd[2], "subsystem" => rexd[7], "vendor" => rexd[3], "subvendor" => rexd[6], "revision" => rexd[5] }
-    puts output
+  `/sbin/lspci -vmm`.each_line do |line|
+    if line.strip.empty?
+      puts "encountered an empty line"
+      puts output
+      output = {}
+    else
+      k, v = line.gsub(/[^0-9A-Za-z\:\.\,]/, '').split(/\:/,2)
+      output[k] = v
+    end
   end
 end
+ 
